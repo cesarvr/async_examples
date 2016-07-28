be4bf5e7df66d222a1b6c35a02a4ab5f38c70257
cvaldezr <cesarv01@yahoo.com>
2016-07-28 10:44:06 +0000

    removing old files

--- test/big_concurrent.sh
+++
@@ -1,4 +0,0 @@
-for i in `seq 1 50000`
-do
-  curl -H "Accept: application/xml" -H "Content-Type: application/xml" -X GET http://localhost:8887  && echo "HTTP Request: $i" &
-done
--- test/concurrent.sh
+++
@@ -1,5 +0,0 @@
-
-#for i in {1..500}
-#do
-  curl -H "Accept: application/xml" -H "Content-Type: application/xml" -X GET http://localhost:8888  #&& echo "HTTP Request: $i" &
-#done
